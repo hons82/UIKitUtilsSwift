c25a1777db56d825fab93e8f412ee31cb705a9af
Hannes Tribus <hannes.tribus@wuerth-phoenix.com>
2015-11-16 11:47:29 +0100

    Initial Commit

---
+++ UIKitUtilsSwift/UIKitUtilsSwift.h
@@ -0,0 +1,19 @@
+//
+//  UIKitUtilsSwift.h
+//  UIKitUtilsSwift
+//
+//  Created by Hannes Tribus on 16/11/15.
+//  Copyright © 2015 3Bus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIKitUtilsSwift.
+FOUNDATION_EXPORT double UIKitUtilsSwiftVersionNumber;
+
+//! Project version string for UIKitUtilsSwift.
+FOUNDATION_EXPORT const unsigned char UIKitUtilsSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitUtilsSwift/PublicHeader.h>
+
+
